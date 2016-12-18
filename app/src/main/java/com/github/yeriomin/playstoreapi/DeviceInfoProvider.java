81fd44ef8098cd5a179b5eacb57718f593de759f
Sergey Eremin <yeriomin@gmail.com>
2016-12-18 05:32:30 +0300

    Moved former google-play-crawler to a separate library

--- app/src/main/java/com/github/yeriomin/playstoreapi/DeviceInfoProvider.java
+++
@@ -1,9 +0,0 @@
-package com.github.yeriomin.playstoreapi;
-
-public interface DeviceInfoProvider {
-
-    AndroidCheckinRequest generateAndroidCheckinRequest();
-    DeviceConfigurationProto getDeviceConfigurationProto();
-    String getUserAgentString();
-    int getSdkVersion();
-}
