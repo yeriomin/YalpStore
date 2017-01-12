e157e38d8f05f474615306e673eae0ebb5e464ed
Sergey Eremin <yeriomin@gmail.com>
2017-01-12 17:15:20 +0300

    Password no longer stored, only used to get token.

--- app/src/main/java/com/github/yeriomin/yalpstore/PreferenceActivity.java
+++ app/src/main/java/com/github/yeriomin/yalpstore/PreferenceActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 public class PreferenceActivity extends android.preference.PreferenceActivity {
 
     public static final String PREFERENCE_EMAIL = "PREFERENCE_EMAIL";
-    public static final String PREFERENCE_PASSWORD = "PREFERENCE_PASSWORD";
     public static final String PREFERENCE_AUTH_TOKEN = "PREFERENCE_AUTH_TOKEN";
     public static final String PREFERENCE_GSF_ID = "PREFERENCE_GSF_ID";
     public static final String PREFERENCE_AUTO_INSTALL = "PREFERENCE_AUTO_INSTALL";
