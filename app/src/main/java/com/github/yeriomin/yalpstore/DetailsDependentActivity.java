62d2ed67f4b2e9e816fe2f584e15975d4f46be25
Sergey Eremin <yeriomin@gmail.com>
2017-03-18 05:08:49 +0300

    Added the third line to details dependent app lists

--- app/src/main/java/com/github/yeriomin/yalpstore/DetailsDependentActivity.java
+++ app/src/main/java/com/github/yeriomin/yalpstore/DetailsDependentActivity.java
@@ -27,6 +27,9 @@ abstract public class DetailsDependentActivity extends AppListActivity {
     protected Map<String, Object> formatApp(App app) {
         Map<String, Object> map = super.formatApp(app);
         map.put(LINE2, getString(R.string.list_line_2_search, app.getInstalls(), app.getRating().getAverage(), app.getUpdated()));
+        String ads = getString(app.containsAds() ? R.string.list_app_has_ads : R.string.list_app_no_ads);
+        String gsf = getString(app.getDependencies().isEmpty() ? R.string.list_app_independent_from_gsf : R.string.list_app_depends_on_gsf);
+        map.put(LINE3, getString(R.string.list_line_3_search, app.getPrice(), ads, gsf));
         map.put(ICON, app.getIconUrl());
         return map;
     }
