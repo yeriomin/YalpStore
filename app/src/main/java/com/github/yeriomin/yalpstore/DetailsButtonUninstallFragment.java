32d0101d143118caf78abd2a2f398dcb7e8cde09
Sergey Eremin <yeriomin@gmail.com>
2017-04-26 22:39:30 +0300

    Issue #145 system app uninstallation

--- app/src/main/java/com/github/yeriomin/yalpstore/DetailsButtonUninstallFragment.java
+++ app/src/main/java/com/github/yeriomin/yalpstore/DetailsButtonUninstallFragment.java
@@ -1,6 +1,9 @@
 package com.github.yeriomin.yalpstore;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +23,12 @@ public class DetailsButtonUninstallFragment extends DetailsButtonFragment {
 
     @Override
     protected boolean shouldBeVisible() {
-        return app.isInstalled();
+        try {
+            activity.getPackageManager().getPackageInfo(app.getPackageName(), 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 
     @Override
@@ -28,8 +36,39 @@ public class DetailsButtonUninstallFragment extends DetailsButtonFragment {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                activity.startActivity(new Intent(Intent.ACTION_DELETE, Uri.parse("package:" + app.getPackageName())));
+                if (isSystemAndReadyForPermanentUninstallation()) {
+                    askAndUninstall();
+                } else {
+                    activity.startActivity(new Intent(Intent.ACTION_DELETE, Uri.parse("package:" + app.getPackageName())));
+                }
             }
         };
+    }
+
+    private boolean isSystemAndReadyForPermanentUninstallation() {
+        return app.isSystem() && app.getPackageInfo().applicationInfo.sourceDir.startsWith("/system/");
+    }
+
+    private void askAndUninstall() {
+        final CheckShellTask checkTask = new CheckShellTask(activity);
+        checkTask.setPrimaryTask(new UninstallSystemAppTask(activity, app));
+        new AlertDialog.Builder(activity)
+            .setMessage(R.string.dialog_message_system_app_warning_uninstall)
+            .setTitle(R.string.dialog_title_system_app_warning)
+            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    checkTask.execute();
+                    dialog.dismiss();
+                }
+            })
+            .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            })
+            .show()
+        ;
     }
 }
