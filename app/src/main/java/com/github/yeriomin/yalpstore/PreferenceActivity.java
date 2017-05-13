2b84b270fb777f6b497488b59081ad1825a0bf92
Sergey Eremin <yeriomin@gmail.com>
2017-05-13 05:21:44 +0300

    Fixing a NumberFormatException

--- app/src/main/java/com/github/yeriomin/yalpstore/PreferenceActivity.java
+++ app/src/main/java/com/github/yeriomin/yalpstore/PreferenceActivity.java
@@ -49,7 +49,11 @@ public class PreferenceActivity extends android.preference.PreferenceActivity {
     }
 
     static public int getUpdateInterval(Context context) {
-        return Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(context).getString(PreferenceActivity.PREFERENCE_BACKGROUND_UPDATE_INTERVAL, "-1"));
+        try {
+            return Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(context).getString(PreferenceActivity.PREFERENCE_BACKGROUND_UPDATE_INTERVAL, "-1"));
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 
     static public boolean canInstallInBackground(Context context) {
