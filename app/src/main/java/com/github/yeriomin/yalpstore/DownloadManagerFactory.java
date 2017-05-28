00c98eb0afc7e090b176068c9090daceeac80ab8
Sergey Eremin <yeriomin@gmail.com>
2017-05-29 01:11:28 +0300

    On Nougat use fallback downloader if VPN is on.

--- app/src/main/java/com/github/yeriomin/yalpstore/DownloadManagerFactory.java
+++ app/src/main/java/com/github/yeriomin/yalpstore/DownloadManagerFactory.java
@@ -1,16 +1,29 @@
 package com.github.yeriomin.yalpstore;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.os.Build;
 import android.util.Log;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import eu.chainfire.libsuperuser.Shell;
 
 public class DownloadManagerFactory {
 
     private static final String DOWNLOAD_MANAGER_PACKAGE_NAME = "com.android.providers.downloads";
 
     static public DownloadManagerInterface get(Context context) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.GINGERBREAD || !nativeDownloadManagerEnabled(context)) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.GINGERBREAD
+            || !nativeDownloadManagerEnabled(context)
+            || nougatVpn()
+        ) {
             Log.i(DownloadManagerFactory.class.getName(), "DownloadManager unavailable - using a fallback");
             return new DownloadManagerFake(context);
         } else {
@@ -36,5 +49,20 @@ public class DownloadManagerFactory {
                 || state == PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER
             );
         }
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    static private boolean nougatVpn() {
+        if (Build.VERSION.SDK_INT != Build.VERSION_CODES.N && Build.VERSION.SDK_INT != Build.VERSION_CODES.N_MR1) {
+            return false;
+        }
+        for (String line: Shell.SH.run("ls -1 /sys/class/net")) {
+            String networkName = line.trim();
+            if (networkName.startsWith("tun") || networkName.startsWith("ppp")) {
+                Log.i(DownloadManagerFactory.class.getName(), "VPN seems to be on: " + networkName);
+                return true;
+            }
+        }
+        return false;
     }
 }
