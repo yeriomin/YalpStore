700fcc9094122d14f7ba2b749e40d29aca7b71d9
Sergey Eremin <yeriomin@gmail.com>
2017-10-17 19:27:56 +0300

    "No updates" message is back

--- app/src/main/java/com/github/yeriomin/yalpstore/task/playstore/ForegroundUpdatableAppsTask.java
+++ app/src/main/java/com/github/yeriomin/yalpstore/task/playstore/ForegroundUpdatableAppsTask.java
@@ -60,6 +60,9 @@ public class ForegroundUpdatableAppsTask extends UpdatableAppsTask implements Cl
             return;
         }
         activity.addApps(updatableApps);
+        if (this.updatableApps.isEmpty()) {
+            this.errorView.setText(R.string.list_empty_updates);
+        }
         toggleUpdateAll(this.updatableApps.size() > 0);
     }
 
