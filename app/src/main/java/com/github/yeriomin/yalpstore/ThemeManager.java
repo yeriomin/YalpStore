c2d1d951ede5232483c69d0ca1fb3024d3eccf70
Sergey Eremin <yeriomin@gmail.com>
2018-01-24 18:55:08 +0300

    Issue #372 Fixed white text on white background in search suggestions on certain ROMs

--- app/src/main/java/com/github/yeriomin/yalpstore/ThemeManager.java
+++ app/src/main/java/com/github/yeriomin/yalpstore/ThemeManager.java
@@ -10,24 +10,35 @@ public class ThemeManager {
     static public void setTheme(Activity activity) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
         String theme = prefs.getString(PreferenceActivity.PREFERENCE_UI_THEME, PreferenceActivity.THEME_NONE);
+        int themeId = getThemeId(theme, activity);
+        if (themeId != 0) {
+            activity.setTheme(themeId);
+        }
+        if (theme.equals(PreferenceActivity.THEME_BLACK)) {
+            activity.getWindow().setBackgroundDrawableResource(android.R.color.black);
+        }
+    }
+
+    static private int getThemeId(String theme, Activity activity) {
         switch (theme) {
             default:
             case PreferenceActivity.THEME_NONE:
-                if (isAmazonTv(activity)) {
-                    activity.setTheme(getThemeDark());
-                }
-                break;
+                return getThemeDefault(activity);
             case PreferenceActivity.THEME_LIGHT:
-                activity.setTheme(getThemeLight());
-                break;
+                return getThemeLight();
             case PreferenceActivity.THEME_DARK:
-                activity.setTheme(getThemeDark());
-                break;
             case PreferenceActivity.THEME_BLACK:
-                activity.setTheme(getThemeDark());
-                activity.getWindow().setBackgroundDrawableResource(android.R.color.black);
-                break;
+                return getThemeDark();
         }
+    }
+
+    static private int getThemeDefault(Activity activity) {
+        if (isAmazonTv(activity)) {
+            getThemeDark();
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return R.style.Theme_DeviceDefault_Almost;
+        }
+        return 0;
     }
 
     static private int getThemeLight() {
