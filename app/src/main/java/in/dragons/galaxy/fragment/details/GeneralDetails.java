11372e0b46b91bd238f3a25d8fd48907a7e18ddb
Mr. Dragon <whyorean@gmail.com>
2018-03-11 20:14:02 +0530

    Show General Card & Set start margin to Beta Card

--- app/src/main/java/in/dragons/galaxy/fragment/details/GeneralDetails.java
+++ app/src/main/java/in/dragons/galaxy/fragment/details/GeneralDetails.java
@@ -60,7 +60,7 @@ public class GeneralDetails extends Abstract {
 
     private void drawGeneralDetails(App app) {
         activity.findViewById(R.id.app_detail).setVisibility(View.VISIBLE);
-
+        activity.findViewById(R.id.general_card).setVisibility(View.VISIBLE);
         setText(R.id.installs, R.string.details_installs, Util.addDiPrefix(app.getInstalls()));
         if (app.isEarlyAccess()) {
             setText(R.id.rating, R.string.early_access);
