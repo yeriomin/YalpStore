ed11d7348e7c14c89e25ce9442da25dbea5b2522
Mr. Dragon <whyorean@gmail.com>
2018-04-05 21:09:26 +0530

    Add Community based Apps

--- app/src/main/java/in/dragons/galaxy/adapters/CommunityBasedAppsAdapter.java
+++ app/src/main/java/in/dragons/galaxy/adapters/CommunityBasedAppsAdapter.java
@@ -1,22 +1,28 @@
 package in.dragons.galaxy.adapters;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 import in.dragons.galaxy.R;
 import in.dragons.galaxy.activities.DetailsActivity;
 
-public class ComunityBasedAppsAdapter extends RecyclerView.Adapter<ComunityBasedAppsAdapter.MyViewHolderInst> {
+public class CommunityBasedAppsAdapter extends RecyclerView.Adapter<CommunityBasedAppsAdapter.MyViewHolderInst> {
 
     String pkg;
     private List<FeaturedHolder> FeaturedAppsH;
+    private Context context;
 
     class MyViewHolderInst extends RecyclerView.ViewHolder {
         TextView cbased_name, cbased_price;
@@ -29,22 +35,20 @@ public class ComunityBasedAppsAdapter extends RecyclerView.Adapter<ComunityBased
             cbased_image = view.findViewById(R.id.cbased_image);
             cbased_price = view.findViewById(R.id.cbased_price);
             cbased_layout = view.findViewById(R.id.cbased_layout);
-            cbased_layout.setOnClickListener(v -> {
-                view.getContext().startActivity(DetailsActivity.getDetailsIntent(view.getContext(), pkg));
-            });
         }
     }
 
 
-    public ComunityBasedAppsAdapter(List<FeaturedHolder> FeaturedAppsH) {
+    public CommunityBasedAppsAdapter(List<FeaturedHolder> FeaturedAppsH, Context context) {
         this.FeaturedAppsH = FeaturedAppsH;
+        this.context = context;
     }
 
+    @NonNull
     @Override
-    public MyViewHolderInst onCreateViewHolder(ViewGroup parent, int viewType) {
+    public MyViewHolderInst onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.comunity_based_adapter, parent, false);
-
         return new MyViewHolderInst(itemView);
     }
 
@@ -52,7 +56,13 @@ public class ComunityBasedAppsAdapter extends RecyclerView.Adapter<ComunityBased
     public void onBindViewHolder(final MyViewHolderInst holder, int position) {
         final FeaturedHolder featuredHolder = FeaturedAppsH.get(position);
         holder.cbased_name.setText(featuredHolder.cbased_name);
-        pkg = featuredHolder.cbased_packagename;
+        holder.cbased_layout.setOnClickListener(v -> {
+            context.startActivity(DetailsActivity.getDetailsIntent(context, featuredHolder.cbased_packagename));
+        });
+        Picasso.with(context)
+                .load(featuredHolder.cbased_appicon)
+                .placeholder(R.drawable.ic_placeholder)
+                .into(holder.cbased_image);
     }
 
     @Override
@@ -63,10 +73,12 @@ public class ComunityBasedAppsAdapter extends RecyclerView.Adapter<ComunityBased
     public static class FeaturedHolder {
         String cbased_name;
         String cbased_packagename;
+        String cbased_appicon;
 
-        public FeaturedHolder(String name, String pkg) {
+        public FeaturedHolder(String name, String pkg, String app_icon) {
             this.cbased_name = name;
             this.cbased_packagename = pkg;
+            this.cbased_appicon = app_icon;
         }
 
     }
--- app/src/main/java/in/dragons/galaxy/view/CommunityBasedApps.java
+++ app/src/main/java/in/dragons/galaxy/view/CommunityBasedApps.java
@@ -5,10 +5,8 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -18,12 +16,9 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-import in.dragons.galaxy.adapters.ComunityBasedAppsAdapter;
+import in.dragons.galaxy.adapters.CommunityBasedAppsAdapter;
 
 public class CommunityBasedApps extends RecyclerView {
-
-    private String JSON_PATH = "https://raw.githubusercontent.com/GalaxyStore/MetaData/master/community_apps.json";
-    private List<ComunityBasedAppsAdapter.FeaturedHolder> FeaturedAppsHolder;
 
     public CommunityBasedApps(Context context) {
         super(context);
@@ -46,15 +41,18 @@ public class CommunityBasedApps extends RecyclerView {
 
     private void JsonParser(Context context) {
         RequestQueue mRequestQueue = Volley.newRequestQueue(context);
-        FeaturedAppsHolder = new ArrayList<>();
+        List<CommunityBasedAppsAdapter.FeaturedHolder> FeaturedAppsHolder = new ArrayList<>();
+        String JSON_PATH = "https://raw.githubusercontent.com/GalaxyStore/MetaData/master/community_apps.json";
         JsonArrayRequest req = new JsonArrayRequest(JSON_PATH,
                 response -> {
                     try {
                         for (int i = 0; i < response.length(); i++) {
                             JSONObject inst = (JSONObject) response.get(i);
-                            FeaturedAppsHolder = new ArrayList<>();
-                            ComunityBasedAppsAdapter adapter = new ComunityBasedAppsAdapter(FeaturedAppsHolder);
-                            ComunityBasedAppsAdapter.FeaturedHolder apps = new ComunityBasedAppsAdapter.FeaturedHolder(inst.getString("app_name"), inst.getString("app_packagename"));
+                            CommunityBasedAppsAdapter adapter = new CommunityBasedAppsAdapter(FeaturedAppsHolder, context);
+                            CommunityBasedAppsAdapter.FeaturedHolder apps = new CommunityBasedAppsAdapter.FeaturedHolder(
+                                    inst.getString("app_name"),
+                                    inst.getString("app_packagename"),
+                                    inst.getString("app_icon"));
                             FeaturedAppsHolder.add(apps);
                             setAdapter(adapter);
                             setLayoutManager(new GridLayoutManager(context, 3));
@@ -63,11 +61,7 @@ public class CommunityBasedApps extends RecyclerView {
                         e.printStackTrace();
                         Log.w("JSON_ERROR", "Error: " + e.getMessage());
                     }
-                }, error -> {
-            VolleyLog.d("Communication", "Error: " + error.getMessage());
-            Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-            Log.w("JSON_ERROR", "Error: " + error.getMessage());
-        });
+                }, error -> Log.w("JSON_ERROR", "Error: " + error.getMessage()));
         mRequestQueue.add(req);
     }
 
