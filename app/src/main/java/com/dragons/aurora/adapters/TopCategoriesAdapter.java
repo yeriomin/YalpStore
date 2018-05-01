d794fda603d259ab54210d4a2f111e59e0f6313c
Mr. Dragon <whyorean@gmail.com>
2018-05-01 13:16:12 +0530

    Make the tap visible

--- app/src/main/java/com/dragons/aurora/adapters/TopCategoriesAdapter.java
+++ app/src/main/java/com/dragons/aurora/adapters/TopCategoriesAdapter.java
@@ -7,29 +7,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.percolate.caffeine.ViewUtils;
 
 import com.dragons.aurora.R;
 import com.dragons.aurora.SharedPreferencesTranslator;
 import com.dragons.aurora.activities.CategoryAppsActivity;
+import com.percolate.caffeine.ViewUtils;
 
 public class TopCategoriesAdapter extends RecyclerView.Adapter<TopCategoriesAdapter.ViewHolder> {
 
     private Context context;
     private String[] categories;
-
-    private Integer[] categoriesImg = {
-            R.drawable.ic_photography,
-            R.drawable.ic_music__audio,
-            R.drawable.ic_entertainment,
-            R.drawable.ic_shopping,
-            R.drawable.ic_personalization,
-            R.drawable.ic_social,
-            R.drawable.ic_communication
-    };
 
     private SharedPreferencesTranslator translator;
 
@@ -37,17 +25,6 @@ public class TopCategoriesAdapter extends RecyclerView.Adapter<TopCategoriesAdap
         this.categories = topCategories;
         this.context = context;
         this.translator = new SharedPreferencesTranslator(PreferenceManager.getDefaultSharedPreferences(context));
-    }
-
-    static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView topLabel;
-        ImageView topImage;
-
-        ViewHolder(View v) {
-            super(v);
-            topLabel = ViewUtils.findViewById(v, R.id.top_cat_name);
-            topImage = ViewUtils.findViewById(v, R.id.top_cat_img);
-        }
     }
 
     @NonNull
@@ -60,12 +37,20 @@ public class TopCategoriesAdapter extends RecyclerView.Adapter<TopCategoriesAdap
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         holder.topLabel.setText(translator.getString(categories[position]));
-        holder.topImage.setImageDrawable(context.getResources().getDrawable(categoriesImg[holder.getAdapterPosition()]));
         holder.topLabel.setOnClickListener(v -> CategoryAppsActivity.start(context, categories[holder.getAdapterPosition()]));
     }
 
     @Override
     public int getItemCount() {
         return categories.length;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView topLabel;
+
+        ViewHolder(View v) {
+            super(v);
+            topLabel = ViewUtils.findViewById(v, R.id.top_cat_name);
+        }
     }
 }
