f713317dd0d5b33448ef2b61d981640bbf3ea2fd
Mr. Dragon <whyorean@gmail.com>
2018-05-04 00:00:31 +0530

    Get screenshot dimention based on device metrics

--- app/src/main/java/com/dragons/aurora/adapters/SmallScreenshotsAdapter.java
+++ app/src/main/java/com/dragons/aurora/adapters/SmallScreenshotsAdapter.java
@@ -2,7 +2,7 @@ package com.dragons.aurora.adapters;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,27 +10,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.dragons.aurora.R;
+import com.dragons.aurora.activities.FullscreenImageActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
-
-import com.dragons.aurora.R;
-import com.dragons.aurora.activities.FullscreenImageActivity;
 
 public class SmallScreenshotsAdapter extends RecyclerView.Adapter<SmallScreenshotsAdapter.MyViewHolderInst> {
     private List<SmallScreenshotsAdapter.Holder> ssholder;
     private Context context;
 
-
-    class MyViewHolderInst extends RecyclerView.ViewHolder {
-        ImageView ss_image;
-
-        MyViewHolderInst(View view) {
-            super(view);
-            ss_image
-                    = view.findViewById(R.id.scrn_itm_s);
-        }
-    }
 
     public SmallScreenshotsAdapter(List<SmallScreenshotsAdapter.Holder> FeaturedAppsH, Context context) {
         this.ssholder = FeaturedAppsH;
@@ -70,6 +59,17 @@ public class SmallScreenshotsAdapter extends RecyclerView.Adapter<SmallScreensho
 
         public Holder(List<String> url) {
             this.url = url;
+        }
+    }
+
+    class MyViewHolderInst extends RecyclerView.ViewHolder {
+        ImageView ss_image;
+
+        MyViewHolderInst(View view) {
+            super(view);
+            ss_image = view.findViewById(R.id.scrn_itm_s);
+            ss_image.getLayoutParams().height = (Resources.getSystem().getDisplayMetrics().heightPixels) / 3;
+            ss_image.getLayoutParams().width = (Resources.getSystem().getDisplayMetrics().widthPixels) / 3;
         }
     }
 }
