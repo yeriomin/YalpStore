yeriomin/YalpStore

commit d888c5d0276aecdf15b0704dfaedc424f1fd0a65

Dialogs generalized

app/src/main/java/com/github/yeriomin/yalpstore/ThemeManagerAbstract.java
@@ -1,6 +1,7 @@
 package com.github.yeriomin.yalpstore;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -9,16 +10,10 @@ import android.util.TypedValue;
 
 abstract public class ThemeManagerAbstract {
 
-    protected Activity activity;
-
     abstract protected int getThemeLight();
     abstract protected int getThemeDark();
 
-    public ThemeManagerAbstract(Activity activity) {
-        this.activity = activity;
-    }
-
-    public void setTheme() {
+    public void setTheme(Activity activity) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
         String theme = prefs.getString(PreferenceActivity.PREFERENCE_UI_THEME, PreferenceActivity.THEME_NONE);
         int themeId = getThemeId(theme, activity);
@@ -30,11 +25,17 @@ abstract public class ThemeManagerAbstract {
         }
     }
 
-    private int getThemeId(String theme, Activity activity) {
+    public int getThemeId(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String theme = prefs.getString(PreferenceActivity.PREFERENCE_UI_THEME, PreferenceActivity.THEME_NONE);
+        return getThemeId(theme, context);
+    }
+
+    private int getThemeId(String theme, Context context) {
         switch (theme) {
             default:
             case PreferenceActivity.THEME_NONE:
-                return getThemeDefault(activity);
+                return getThemeDefault(context);
             case PreferenceActivity.THEME_LIGHT:
                 return getThemeLight();
             case PreferenceActivity.THEME_DARK:
@@ -43,23 +44,23 @@ abstract public class ThemeManagerAbstract {
         }
     }
 
-    private int getThemeDefault(Activity activity) {
-        if (isAmazonTv(activity)) {
+    private int getThemeDefault(Context context) {
+        if (isAmazonTv(context)) {
             getThemeDark();
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return isWindowBackgroundDark(activity) ? getThemeDark() : getThemeLight();
+            return isWindowBackgroundDark(context) ? getThemeDark() : getThemeLight();
         }
         return 0;
     }
 
-    static private boolean isAmazonTv(Activity activity) {
-        return ((YalpStoreApplication) activity.getApplication()).isTv() && Build.MANUFACTURER.toLowerCase().contains("amazon");
+    static private boolean isAmazonTv(Context context) {
+        return ((YalpStoreApplication) context.getApplicationContext()).isTv() && Build.MANUFACTURER.toLowerCase().contains("amazon");
     }
 
-    static boolean isWindowBackgroundDark(Activity activity) {
+    static boolean isWindowBackgroundDark(Context context) {
         TypedValue color = new TypedValue();
         try {
-            activity.getTheme().resolveAttribute(android.R.attr.colorBackground, color, true);
+            context.getTheme().resolveAttribute(android.R.attr.colorBackground, color, true);
             return Color.red(color.data) < 128 && Color.green(color.data) < 128 && Color.blue(color.data) < 128;
         } catch (RuntimeException e) {
             return false;

app/src/contemporary/java/com/github/yeriomin/yalpstore/BaseActivity.java
@@ -124,7 +124,7 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     public void setContentView(int layoutResID) {
-        new ThemeManager(this).setTheme();
+        new ThemeManager().setTheme(this);
         super.setContentView(wrapperLayoutResId);
         ViewGroup container = findViewById(R.id.specific_layout_container);
         View specificLayout = getLayoutInflater().inflate(layoutResID, container, false);

app/src/contemporary/java/com/github/yeriomin/yalpstore/ThemeManager.java
@@ -1,12 +1,6 @@
 package com.github.yeriomin.yalpstore;
 
-import android.app.Activity;
-
 public class ThemeManager extends ThemeManagerAbstract {
-
-    public ThemeManager(Activity activity) {
-        super(activity);
-    }
 
     protected int getThemeLight() {
         return R.style.YalpStoreThemeLight;

app/src/contemporary/java/com/github/yeriomin/yalpstore/view/DialogWrapper.java
@@ -1,0 +1,133 @@
+package com.github.yeriomin.yalpstore.view;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.ListAdapter;
+
+public class DialogWrapper extends DialogWrapperAbstract {
+
+    protected AlertDialog.Builder builder;
+    protected AlertDialog dialog;
+
+    public DialogWrapper(Activity activity) {
+        super(activity);
+        builder = new AlertDialog.Builder(activity);
+    }
+
+    @Override
+    public DialogWrapperAbstract setMessage(int stringResId) {
+        builder.setMessage(stringResId);
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract setMessage(CharSequence message) {
+        builder.setMessage(message);
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract setTitle(int stringResId) {
+        builder.setTitle(stringResId);
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract setTitle(CharSequence title) {
+        builder.setTitle(title);
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract setIcon(Drawable icon) {
+        builder.setIcon(icon);
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract setPositiveButton(int stringResId, OnClickListener listener) {
+        builder.setPositiveButton(stringResId, listener);
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract setNegativeButton(int stringResId, OnClickListener listener) {
+        builder.setNegativeButton(stringResId, listener);
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract setView(View view) {
+        builder.setView(view);
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract setLayout(int layoutResId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder.setView(layoutResId);
+        } else {
+            builder.setView(activity.getLayoutInflater().inflate(layoutResId, null));
+        }
+        return this;
+    }
+
+    @Override
+    public View findViewById(int viewId) {
+        if (null == dialog) {
+            create();
+        }
+        return dialog.findViewById(viewId);
+    }
+
+    @Override
+    public DialogWrapperAbstract setItems(int arrayResId, DialogInterface.OnClickListener listener) {
+        builder.setItems(arrayResId, listener);
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract setAdapter(ListAdapter listAdapter, OnClickListener listener) {
+        builder.setAdapter(listAdapter, listener);
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract setCancelable(boolean cancelable) {
+        builder.setCancelable(cancelable);
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract create() {
+        dialog = builder.create();
+        return this;
+    }
+
+    @Override
+    public DialogWrapperAbstract show() {
+        if (null == dialog) {
+            create();
+        }
+        dialog.show();
+        return this;
+    }
+
+    @Override
+    public void cancel() {
+        if (null != dialog) {
+            dialog.cancel();
+        }
+    }
+
+    @Override
+    public void dismiss() {
+        if (null != dialog) {
+            dialog.dismiss();
+        }
+    }
+}

app/src/contemporary/res/drawable-v21/button_border.xml
@@ -1,7 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<shape
-    xmlns:android="http://schemas.android.com/apk/res/android"
-    android:shape="rectangle">
-    <corners android:radius="3dp" />
-    <stroke android:width="5px" android:color="?attr/colorAccent" />
-</shape>

app/src/contemporary/res/drawable/button_border.xml
@@ -3,5 +3,5 @@
     xmlns:android="http://schemas.android.com/apk/res/android"
     android:shape="rectangle">
     <corners android:radius="3dp" />
-    <stroke android:width="2dp" android:color="#357ec7" />
+    <stroke android:width="2dp" android:color="@color/color_accent" />
 </shape>

app/src/contemporary/res/drawable/ic_menu.xml
@@ -4,6 +4,6 @@
         android:viewportWidth="24.0"
         android:viewportHeight="24.0">
     <path
-        android:fillColor="#808080"
+        android:fillColor="@color/gray50"
         android:pathData="M3,18h18v-2L3,16v2zM3,13h18v-2L3,11v2zM3,6v2h18L21,6L3,6z"/>
 </vector>

app/src/contemporary/res/values-v21/style.xml
@@ -9,6 +9,14 @@
         <item name="android:windowTranslucentStatus">true</item>
         <item name="android:windowDrawsSystemBarBackgrounds">true</item>
         <item name="android:statusBarColor">@android:color/transparent</item>
+        <item name="android:dialogTheme">@style/YalpStoreDialogStyleLight</item>
+        <item name="android:alertDialogTheme">@style/YalpStoreAlertDialogStyleLight</item>
+    </style>
+    <style name="YalpStoreDialogStyleLight" parent="Theme.AppCompat.Light.Dialog">
+        <item name="colorAccent">#357ec7</item>
+    </style>
+    <style name="YalpStoreAlertDialogStyleLight" parent="Theme.AppCompat.Light.Dialog.Alert">
+        <item name="colorAccent">#357ec7</item>
     </style>
     <style name="YalpStoreThemeDark" parent="Theme.AppCompat.NoActionBar">
         <item name="colorAccent">#357ec7</item>
@@ -19,5 +27,13 @@
         <item name="android:windowTranslucentStatus">true</item>
         <item name="android:windowDrawsSystemBarBackgrounds">true</item>
         <item name="android:statusBarColor">@android:color/transparent</item>
+        <item name="android:dialogTheme">@style/YalpStoreDialogStyleDark</item>
+        <item name="android:alertDialogTheme">@style/YalpStoreAlertDialogStyleDark</item>
+    </style>
+    <style name="YalpStoreDialogStyleDark" parent="Theme.AppCompat.Dialog">
+        <item name="colorAccent">#357ec7</item>
+    </style>
+    <style name="YalpStoreAlertDialogStyleDark" parent="Theme.AppCompat.Dialog.Alert">
+        <item name="colorAccent">#357ec7</item>
     </style>
 </resources>

commit ebb738bcbb3ba99bee8b4ef74b589a2b248dbc2d

fix theme issue on Amazon devices introduced in d888c5d

app/src/main/java/com/github/yeriomin/yalpstore/ThemeManagerAbstract.java
@@ -78,7 +78,7 @@ abstract public class ThemeManagerAbstract {
 
     private int getThemeDefault(Context context) {
         if (isAmazonTv(context)) {
-            getThemeDark();
+            return getThemeDark();
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             return isWindowBackgroundDark(context) ? getThemeDark() : getThemeLight();
         }